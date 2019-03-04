a8c623fb066de3a334c74c8ce0c4339ed2433c71
Ryuma Noma <ryuma20020722@gmail.com>
2019-03-04 23:27:21 +0900

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+//ログイン処理
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NTUyMTMxMDYzOTM0MjIyMzQx.D17D2A.QJEjc89Z77gqb-MWaG9LZiuKERw';
+client.on('ready', () => {
+    console.log('ready...');
+});
+//Bot自身の発言を無視する呪い
+client.on('message', message =>{
+    if(message.author.bot){
+        return;
+   }
+//↓ここに後述のコードをコピペする↓
+   if (message.content === '寒いね') {
+        let channel = message.channel;
+        let author = message.author.username;
+        let reply_text =`おまんこ`;
+        message.reply(reply_text)
+            .then(message => console.log(`Sent message: ${reply_text}`))
+            .catch(console.error);
+        return;
+    }
+    if (message.content.match(/discord.gg/)) {
+        message.delete(100)
+   }
+//↑ここに後述のコードをコピペする↑
+});
+client.login(token);
